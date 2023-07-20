b340c172548413d16b3d0e452bdc29110fa5b510
sebastienserre <sebastien@thivinfo.com>
2023-07-20 10:44:05 +0200

    Define constant if not defined

--- thfo-intranet.php
+++ thfo-intranet.php
@@ -108,6 +108,19 @@ function activate() {
 
 add_filter( 'upload_dir', 'ThfoIntranet\chg_media_dir' );
 function chg_media_dir( $uploads ) {
+
+	$uploads_dir       = wp_upload_dir();
+	$protected_folder = '/intranet/protected';
+	$media_uploads_dir = $uploads_dir['basedir'] . $protected_folder;
+
+	if ( ! defined( 'THFO_MEDIA_UPLOAD' ) ){
+		define( 'THFO_MEDIA_UPLOAD', $media_uploads_dir );
+
+	}
+	if ( ! defined( 'THFO_MEDIA_UPLOAD_URL' ) ){
+		define( 'THFO_MEDIA_UPLOAD_URL', $uploads_dir['baseurl'] . $protected_folder );
+	}
+
 	if ( ! empty( $_REQUEST['data'] ) && 'intranet' === get_post_type( $_REQUEST['data']['wp-refresh-post-lock']['post_id'] ) ) {
 		$uploads['path'] = THFO_MEDIA_UPLOAD . $uploads['subdir'];
 		$uploads['url']  = THFO_MEDIA_UPLOAD_URL . $uploads['subdir'];
